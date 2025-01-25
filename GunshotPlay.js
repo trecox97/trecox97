6983d35f261c04162c0ef607890b7a6b4bb8c0d0
trecox97 <trecox97@gmail.com>
2025-01-24 19:23:59 -0500

    Messed around in the random tab

--- GunshotPlay.js
+++ GunshotPlay.js
@@ -7,7 +7,7 @@ function playGunshot() {
   var newImage = document.createElement("img");
   newImage.src = "Images/gunshot.png";  // Set the image source
   newImage.alt = "Gunshot";             // Add alt text
-  newImage.style.width = "300px";       // Set image size (optional)
+  newImage.style.width = "100px";       // Set image size (optional)
   newImage.style.height = "auto";       // Maintain aspect ratio
 
   // Append the image to the content div
