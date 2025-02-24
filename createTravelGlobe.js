dbcfc496ef339e5c5ed786bc65ac9756743197bc
trecox97 <trecox97@gmail.com>
2025-02-23 21:45:41 -0500

    Spin the globe and edit starting view

--- createTravelGlobe.js
+++ createTravelGlobe.js
@@ -417,23 +417,36 @@ const arcsData = [
 const globeContainer = document.getElementById('globeViz');
 
 // Create the Globe instance
+const minDashLength = 0.4;
+const maxDashLength = 1;
+const minDashGap = 0.1;
+const maxDashGap = 0.5;
+
 const globe = new Globe(globeContainer)
   .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
   .arcsData(arcsData)
   .arcColor('color')
-  .arcDashLength(() => Math.random())
-  .arcDashGap(() => Math.random())
+  .arcDashLength(() => Math.random() * (maxDashLength - minDashLength) + minDashLength)
+  .arcDashGap(() => Math.random() * (maxDashGap - minDashGap) + minDashGap)
   .arcDashAnimateTime([2500])
   .arcStroke([".5px"]);
-  //.arcDashLength([".2"])
+  //  .arcDashLength(() => Math.random() * (maxDashLength - minDashLength) + minDashLength)
+
 
 // Function to update the globe size on window resize
 function resizeGlobe() {
-  globeContainer.style.width = window.innerWidth - 80 + "px";
+  globeContainer.style.width = window.innerWidth - 120 + "px";
   globeContainer.style.height = window.innerHeight + "px";
-  globe.width([window.innerWidth - 80]);
+  globe.width([window.innerWidth - 120]);
   globe.height([window.innerHeight]);
 }
+
+    // Auto-rotate
+    globe.controls().autoRotate = true;
+    globe.controls().autoRotateSpeed = .5;
+
+    //Starting point of view
+    globe.pointOfView({lat: 22, lng: 25, altitude: 2.5 });
 
 // Resize on window load and resize event
 window.addEventListener('resize', resizeGlobe);
