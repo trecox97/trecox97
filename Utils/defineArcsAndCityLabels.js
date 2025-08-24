42ee312fccf61a0288d72f0982569c87ab10bf24
trecox97 <trecox97@gmail.com>
2025-08-23 21:09:29 -0400

    Fixed dropdown bug

--- Utils/defineArcsAndCityLabels.js
+++ Utils/defineArcsAndCityLabels.js
@@ -63,70 +63,70 @@ const arcs2025RoadTrip = [
     startLng: -119.8821,
     endLat: 39.5296, // Reno, NV
     endLng: -119.8138,
-    color: ["blue", "white"]
+    color: ["blue", "gold"]
   },
   {
     startLat: 39.5296, // Reno, NV
     startLng: -119.8138,
     endLat: 40.6461, // Park City, UT
     endLng: -111.4980,
-    color: ["blue", "white"]
+    color: ["gold", "white"]
   },
   {
     startLat: 40.6461, // Park City, UT
     startLng: -111.4980,
     endLat: 39.6654, // Red Rocks, CO
     endLng: -105.2057,
-    color: ["blue", "white"]
+    color: ["white", "red"]
   },
   {
     startLat: 39.6654, // Red Rocks, CO
     startLng: -105.2057,
     endLat: 41.1403, // North Platte, NE
     endLng: -100.7601,
-    color: ["blue", "white"]
+    color: ["red", "green"]
   },
   {
     startLat: 41.1403, // North Platte, NE
     startLng: -100.7601,
     endLat: 39.0997, // Kansas City, MO
     endLng: -94.5786,
-    color: ["blue", "white"]
+    color: ["green", "lightblue"]
   },
   {
     startLat: 39.0997, // Kansas City, MO
     startLng: -94.5786,
     endLat: 38.6270, // St. Louis, MO
     endLng: -90.1994,
-    color: ["blue", "white"]
+    color: ["lightblue", "blue"]
   },
   {
     startLat: 38.6270, // St. Louis, MO
     startLng: -90.1994,
     endLat: 39.9612, // Columbus, OH
     endLng: -82.9988,
-    color: ["blue", "white"]
+    color: ["blue", "green"]
   },
   {
     startLat: 39.9612, // Columbus, OH
     startLng: -82.9988,
     endLat: 40.4406, // Pittsburgh, PA
     endLng: -79.9959,
-    color: ["blue", "white"]
+    color: ["green", "yellow"]
   },
   {
     startLat: 40.4406, // Pittsburgh, PA
     startLng: -79.9959,
     endLat: 41.0037, // East Stroudsburg, PA
     endLng: -75.1820,
-    color: ["blue", "white"]
+    color: ["yellow", "blue"]
   },
   {
     startLat: 41.0037, // East Stroudsburg, PA
     startLng: -75.1820,
     endLat: 40.8137, // Greenvale, NY
     endLng: -73.6266,
-    color: ["blue", "white"]
+    color: ["blue", "orange"]
   }
 ];
 
@@ -626,9 +626,9 @@ const labels2025RoadTripCities = [
   { lat: 39.6654, lng: -105.2057, name: "Red Rocks", size: 1.2 },
   { lat: 41.1403, lng: -100.7601, name: "North Platte", size: 1.2 },
   { lat: 39.0997, lng: -94.5786, name: "Kansas City", size: 1.2 },
-  { lat: 38.6270, lng: -90.1994, name: "St. Louis", size: 1.2 },
+  { lat: 38.6270, lng: -90.1994, name: "St. Louis", size: 1},
   { lat: 39.9612, lng: -82.9988, name: "Columbus", size: 1.2 },
-  { lat: 40.4406, lng: -79.9959, name: "Pittsburgh", size: 1.2 },
+  { lat: 40.4406, lng: -79.9959, name: "Pittsburgh", size: 1 },
   { lat: 41.0037, lng: -75.1820, name: "East Stroudsburg", size: 1.2 }
 ];
 
@@ -717,7 +717,7 @@ const datasets = {
   "2025 Road Trip": {
     arcs: arcs2025RoadTrip,
     labels: labels2025RoadTripCities,
-    view: { lat: 38.1888, lng: -119.8821, altitude: 1.5 } // Pinecrest Coords
+    view: { lat: 39.0997, lng: -94.5786, altitude: 1.5 } // Kansas City Coords
   },
   "2025 Alaska Cruise": {
     arcs: arcsAlaskaTrip,
